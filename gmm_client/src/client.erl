2b0be7f35a34b56b6daf0dad81d341b482d34213
Piotr Świderski <piotrswiderski@localhost.localdomain>
2021-05-10 23:12:01 +0200

    client module finished

--- gmm_client/src/client.erl
+++ gmm_client/src/client.erl
@@ -11,17 +11,17 @@
 
 
 %% API
--export([getIdList/0, getIdNumber/0, getUserInfo/1, deleteUser/1, addUser/1, test/0]).
+-export([get_id_list/0, get_id_number/0, get_user_info/1, delete_user/1, add_user/1, test/0]).
 
 test()->
   ok.
 
-urlAddress() ->
-  <<"localhost:8080/">>.
+url_address() ->
+  "localhost:8080/".
 
 % 
 
-getRequest(URL)->
+get_request(URL)->
   Method = get,
   Headers = [],
   Payload = <<>>,
@@ -30,54 +30,54 @@ getRequest(URL)->
   {ok, StatusCode, RepsHeaders, ClientRef}.
 
 % reads body of get request
-readBody(ClientRef) ->
+read_body(ClientRef) ->
   {ok, _} = hackney:body(ClientRef).
 
 
 % Get Id List Section
 
-getIdList() ->
+get_id_list() ->
   application:ensure_all_started(hackney),
-  URL = urlAddress()+"users",
-  {ok, _, _, ClientRef} = getRequest(URL),
-  {ok, Body} = readBody(ClientRef),
-  listBodyElements(binary:bin_to_list(Body)).
+  URL = url_address()++"users",
+  {ok, _, _, ClientRef} = get_request(URL),
+  {ok, Body} = read_body(ClientRef),
+  list_body_elements(binary:bin_to_list(Body)).
 
-getIdNumber()->
+get_id_number()->
   application:ensure_all_started(hackney),
-  URL = urlAddress()+"users",
-  {ok, _, _, ClientRef} = getRequest(URL),
-  {ok, Body} = readBody(ClientRef),
-  countBodyElements(binary:bin_to_list(Body), 0).
+  URL = url_address()++"users",
+  {ok, _, _, ClientRef} = get_request(URL),
+  {ok, Body} = read_body(ClientRef),
+  count_body_elements(binary:bin_to_list(Body), 0).
 
-countBodyElements(Body, Acc) ->
+count_body_elements(Body, Acc) ->
   case Body of
     [] -> Acc;
     Body ->
       [_ | Tail] = string:split(Body, ","),
-      countBodyElements(Tail, Acc + 1)
+      count_body_elements(Tail, Acc + 1)
   end.
 
 % Get User Info Section
 
-getUserInfo(Id)->
+get_user_info(Id)->
   application:ensure_all_started(hackney),
-  URL = urlAddress()+"users/"+Id,
-  {ok, _, _, ClientRef} = getRequest(URL),
-  {ok, Body} = readBody(ClientRef),
+  URL = url_address()++"users/"++Id,
+  {ok, _, _, ClientRef} = get_request(URL),
+  {ok, Body} = read_body(ClientRef),
   binary:bin_to_list(Body).
 
-listBodyElements(Body) ->
+list_body_elements(Body) ->
   case Body of
     [] -> [];
     Body ->
       [Head | Tail] = string:split(Body, ","),
       io:format("~s~n", [Head]),
-      listBodyElements(Tail)
+      list_body_elements(Tail)
   end.
 
 % Delete User Section
-deleteRequest(URL)->
+delete_request(URL)->
   Method = delete,
   Headers = [],
   Payload = <<>>,
@@ -85,28 +85,21 @@ deleteRequest(URL)->
   {ok, StatusCode, RepsHeaders, ClientRef} = hackney:request(Method, URL, Headers, Payload, Options),
   {ok, StatusCode, RepsHeaders, ClientRef}.
 
-deleteUser(Id)->
+delete_user(Id)->
   application:ensure_all_started(hackney),
-  URL = urlAddress()+"users/"+Id,
-  {ok, _, _, _} = deleteRequest(URL).
-
-
-% 3) POST - dodaj uzytkownika
-% curl -i -X POST localhost:8080/users --header "Content-Type: application/json" -d '{"name": "Karol"}'
+  URL = url_address()++"users/"++Id,
+  {ok, _, _, _} = delete_request(URL).
 
 % Add User Section
-addUser(Name)->
+add_user(Name)->
   application:ensure_all_started(hackney),
-  Transport = hackney_ssl,
-  Host = << "localhost:8080/users" >>,
-  Port = 443,
-  Options = [],
-  {ok, ConnRef} = hackney:connect(Transport, Host, Port, Options),
-  PostBody = "{	\"name\": \""+Name+"\"}",
-  ReqBody = <<PostBody>>,
+
+  PostBody = "{	\"name\": \""++Name++"\"}",
+  ReqBody = list_to_binary(PostBody),
   ReqHeaders = [{<<"Content-Type">>, <<"application/json">>}],
-  NextPath = <<"localhost:8080/users">>,
-  NextMethod = post,
-  NextReq = {NextMethod, NextPath, ReqHeaders, ReqBody},
-  {ok, _, _, ConnRef} = hackney:send_request(ConnRef, NextReq),
-  {ok, _} = hackney:body(ConnRef).
+  Path = <<"localhost:8080/users">>,
+  Method = post,
+  {ok, ClientRef} = hackney:request(Method, Path, ReqHeaders, stream, []),
+  ok  = hackney:send_body(ClientRef, ReqBody),
+  {ok, _Status, _Headers, ClientRef} = hackney:start_response(ClientRef),
+  {ok, _} = hackney:body(ClientRef).
