767318b7a9403bd3ad996ca6359e3306f3fe56d8
Daniel Jodłoś <danjod99@outlook.com>
2021-05-18 09:19:38 +0200

    Refactoring persistence module
    - Removed server argument
    - Moved graph modification to another module

---
+++ src/graph.erl
@@ -0,0 +1,62 @@
+-module(graph).
+-author("Daniel Jodłoś").
+
+-export([
+    get_user/1, 
+    get_users/0, 
+    add_user/1, 
+    delete_user/1, 
+    update_user/2,
+    add_user_to_group/2,
+    is_member_of_group/2,
+    list_group_users/1,
+    remove_user_from_group/2
+]).
+
+get_user(Id) ->
+    persistence:get(Id).
+
+get_users()->
+    persistence:keys("*").
+
+add_user(Name) ->
+    Id = << <<Y>> ||<<X:4>> <= crypto:hash(md5, term_to_binary(make_ref())), Y <- integer_to_list(X,16)>>,
+    Json = json_utils:encode(#{<<"id">> => Id, <<"Name">> => Name}),
+    Response = persistence:set(Id, Json),
+    case Response of
+        {error, Reason} -> {error, Reason};
+        {ok, _Result} -> {ok, Id}
+    end.
+
+update_user(Id, NewName) ->
+    Json = json_utils:encode(#{<<"id">> => Id, <<"Name">> => NewName}),
+    Response = persistence:set(Id, Json),
+    case Response of
+        {error, Reason} -> {error, Reason};
+        {ok, _Result} -> ok
+    end.
+
+delete_user(Id) ->
+    Response = persistence:del(Id),
+    case Response of
+        {error, Reason} -> {error, Reason};
+        {ok, _Result} -> ok
+    end.
+
+children_key(Key) when is_binary(Key) == true ->
+    <<Key/binary, "/children">>;
+children_key(Key) -> children_key(binary:list_to_bin(Key)).
+
+add_user_to_group(User, Group) ->
+    persistence:set_add(children_key(Group), User).
+
+remove_user_from_group(User, Group) ->
+    persistence:set_remove(children_key(Group), User).
+
+is_member_of_group(User, Group) ->
+    {ok, Result} = persistence:set_is_member(children_key(Group), User),
+    Result.
+
+list_group_users(Group) ->
+    {ok, Result} = persistence:set_list_members(children_key(Group)),
+    Result.
--- src/persistence.erl
+++ src/persistence.erl
@@ -18,16 +18,16 @@
     terminate/2, 
     code_change/3
 ]).
+
 -export([
-    get_user/2, 
-    get_users/1, 
-    add_user/2, 
-    delete_user/2, 
-    update_user/3,
-    add_user_to_group/2,
-    is_member_of_group/2,
-    list_group_users/1,
-    remove_user_from_group/2
+    get/1,
+    set/2,
+    del/1,
+    keys/1,
+    set_add/2,
+    set_remove/2,
+    set_is_member/2,
+    set_list_members/1
 ]).
 
 %% gen_server api
@@ -93,17 +93,17 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% redis api
 
-get(ServerName, Key) ->
-    gen_server:call(ServerName, {get, Key}).
+get(Key) ->
+    gen_server:call(?REDIS_SERVER, {get, Key}).
 
-set(ServerName, Key, Value) ->
-    gen_server:call(ServerName, {set, Key, Value}).
+set(Key, Value) ->
+    gen_server:call(?REDIS_SERVER, {set, Key, Value}).
 
-del(ServerName, Key) ->
-    gen_server:call(ServerName, {del, Key}).
+del(Key) ->
+    gen_server:call(?REDIS_SERVER, {del, Key}).
 
-keys(ServerName, Pattern) ->
-    gen_server:call(ServerName, {keys, Pattern}).
+keys(Pattern) ->
+    gen_server:call(?REDIS_SERVER, {keys, Pattern}).
 
 set_add(Key, Value) ->
     gen_server:call(?REDIS_SERVER, {set_add, Key, Value}).
@@ -116,53 +116,3 @@ set_is_member(Key, Value) ->
 
 set_list_members(Key) ->
     gen_server:call(?REDIS_SERVER, {set_list_members, Key}).
-
-%% persistence higher level api
-
-get_user(ServerName, Id) ->
-    get(ServerName, Id).
-
-get_users(ServerName) ->
-    keys(ServerName, "*").
-
-add_user(ServerName, Name) ->
-    Id = << <<Y>> ||<<X:4>> <= crypto:hash(md5, term_to_binary(make_ref())), Y <- integer_to_list(X,16)>>,
-    Json = json_utils:encode(#{<<"id">> => Id, <<"Name">> => Name}),
-    Response = set(ServerName, Id, Json),
-    case Response of
-        {error, Reason} -> {error, Reason};
-        {ok, _Result} -> {ok, Id}
-    end.
-
-update_user(ServerName, Id, NewName) ->
-    Json = json_utils:encode(#{<<"id">> => Id, <<"Name">> => NewName}),
-    Response = set(ServerName, Id, Json),
-    case Response of
-        {error, Reason} -> {error, Reason};
-        {ok, _Result} -> ok
-    end.
-
-delete_user(ServerName, Id) ->
-    Response = del(ServerName, Id),
-    case Response of
-        {error, Reason} -> {error, Reason};
-        {ok, _Result} -> ok
-    end.
-
-children_key(Key) when is_binary(Key) == true ->
-    <<Key/binary, "/children">>;
-children_key(Key) -> children_key(binary:list_to_bin(Key)).
-
-add_user_to_group(User, Group) ->
-    set_add(children_key(Group), User).
-
-remove_user_from_group(User, Group) ->
-    set_remove(children_key(Group), User).
-
-is_member_of_group(User, Group) ->
-    {ok, Result} = set_is_member(children_key(Group), User),
-    Result.
-
-list_group_users(Group) ->
-    {ok, Result} = set_list_members(children_key(Group)),
-    Result.
