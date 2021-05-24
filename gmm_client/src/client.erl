9b1302cadadd54207c5e836933c579adab734f4b
Petros9 <piotrs1999@gmail.com>
2021-05-24 14:10:54 -0700

    client adjusted

--- gmm_client/src/client.erl
+++ gmm_client/src/client.erl
@@ -11,59 +11,84 @@
 
 
 %% API
--export([get_id_list/0, 
-          get_user_info/1, 
-          delete_user/1, 
-          add_user/1]).
+-export([add_user/1, add_group/1, add_space/1, add_provider/1, 
+  get_vertices_list/0, get_edges_list/0, get_vertex_info/1, delete_vertex/1,
+  add_edge/3, get_edge_info/2, get_vertex_neighbours/2 ,delete_edge/2]).
 
 % CONST
 -define(URL, "localhost:8080/").
 
-simple_request(Method, Url) -> request(Method, Url, [], <<>>, []).
-
-request(Method, Url, Headers, Payload, Options )->
-  {ok, _, _, _} = hackney:request(Method, Url, Headers, Payload, Options).
-
 % Get Id List Section
-
-get_id_list() ->
-  application:ensure_all_started(hackney),
-  Url = ?URL++"users",
-  {ok, _, _, ClientRef} = simple_request(get, Url),
-  {ok, Body} = hackney:body(ClientRef),
-  list_body_elements(binary:bin_to_list(Body)).
-
-% Get User Info Section
-
-get_user_info(Id)->
-  application:ensure_all_started(hackney),
-  Url = ?URL++"users/"++Id,
-  {ok, _, _, ClientRef} = simple_request(get, Url),
-  {ok, Body} = hackney:body(ClientRef),
-  binary:bin_to_list(Body).
-
 list_body_elements(Body) ->
   Format = fun(Element) -> io:format("~s~n", [Element]) end,
   lists:foreach(Format, string:split(Body, ",")).
 
+% VERTICES
 
-% Delete User Section
-
-delete_user(Id)->
-  application:ensure_all_started(hackney),
-  Url = ?URL++"users/"++Id,
-  {ok, _, _, _} = simple_request(delete, Url).
-
-% Add User Section
 add_user(Name)->
   application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?type=user&name="++Name,
+  client_requests:post_request(list_to_binary(Url)).
 
-  PostBody = "{	\"name\": \""++Name++"\"}",
-  ReqBody = list_to_binary(PostBody),
-  ReqHeaders = [{<<"Content-Type">>, <<"application/json">>}],
-  Path = <<"localhost:8080/users">>,
-  {ok, ClientRef} = hackney:request(post, Path, ReqHeaders, stream, []),
-  ok  = hackney:send_body(ClientRef, ReqBody),
-  {ok, _Status, _Headers, ClientRef} = hackney:start_response(ClientRef),
-  {ok, _} = hackney:body(ClientRef).
+add_group(Name)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?type=group&name="++Name,
+  client_requests:post_request(list_to_binary(Url)).
 
+add_space(Name)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?type=space&name="++Name,
+  client_requests:post_request(list_to_binary(Url)).
+
+add_provider(Name)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?type=provider&name="++Name,
+  client_requests:post_request(list_to_binary(Url)).
+
+get_vertices_list()->
+  application:ensure_all_started(hackney),
+  {ok, Body} = client_requests:get_simple_request_body(?URL++"graph/vertices"),
+  list_body_elements(binary:bin_to_list(Body)).
+
+% do ewentualnej serializacji
+get_vertex_info(Id)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?id="++Id,
+  {ok, Body} = client_requests:get_delete_request(Url, get),
+  list_body_elements(binary:bin_to_list(Body)).
+
+
+delete_vertex(Id)->
+  application:ensure_all_started(hackney),
+  Url = Url= ?URL++"graph/vertices?id="++Id,
+  client_requests:get_delete_request(Url, delete).
+
+% EDGES
+
+get_edges_list()->
+  application:ensure_all_started(hackney),
+  {ok, Body} = client_requests:get_simple_request_body(?URL++"graph/edges"),
+  list_body_elements(binary:bin_to_list(Body)).
+
+add_edge(Parent, Child, Permissions)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/edges?parent="++Parent++"&child="++Child++"&permissions="++Permissions,
+  client_requests:post_request(list_to_binary(Url)).
+
+% do zmiany
+get_vertex_neighbours(Vertex, WhichEgdes)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/edges?vertex="++Vertex++"&which_edges="++WhichEgdes,
+  {ok, Body} = client_requests:get_delete_request(Url, get),
+  list_body_elements(binary:bin_to_list(Body)).
+
+get_edge_info(Parent, Child)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/edges?parent="++Parent++"&child="++Child,
+  {ok, Body} = client_requests:get_delete_request(Url, get),
+  list_body_elements(binary:bin_to_list(Body)).
+
+delete_edge(Parent, Child)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/edges?parent="++Parent++"&child="++Child,
+  client_requests:get_delete_request(Url, delete).
