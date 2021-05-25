d6c0fa8e8f61bbdf01d26b8a51880326a7d63a37
Petros9 <piotrs1999@gmail.com>
2021-05-25 04:47:41 -0700

    vertices testes works

--- gmm_client/src/client_test.erl
+++ gmm_client/src/client_test.erl
@@ -11,3 +11,25 @@
 
 -include_lib("eunit/include/eunit.hrl").
 -compile(export_all).
+% users, spaces, providers, group
+vertices_test()->
+    client:add_user("Jan"),
+    client:add_group("Friends"),
+    client:add_space("Space"),
+    client:add_provider("Provider"),
+    UserId = client_test_helper:get_id_from_the_list(),
+    [
+      ?assertEqual("Jan", client_test_helper:get_vertex_name(UserId))
+    ],
+    client:delete_vertex(UserId),
+    SpaceId = client_test_helper:get_id_from_the_list(),
+    [
+      ?assertEqual("Space", client_test_helper:get_vertex_name(SpaceId))
+    ],
+    client:delete_vertex(SpaceId).
+
+
+edges_test()->
+    [
+        ]
+    .
---
+++ gmm_client/src/client_test_helper.erl
@@ -0,0 +1,45 @@
+%%%-------------------------------------------------------------------
+%%% @author Piotr Świderski
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. maj 2021 12:54
+%%%-------------------------------------------------------------------
+-module(client_test_helper).
+-author("Piotr Świderski").
+
+-export([get_id_from_the_list/0, get_vertex_name/1]).
+
+% CONST
+-define(URL, "localhost:8080/").
+
+get_id(List)->
+  [Head | Tail] = List,
+    
+  case "zone" -- Head of
+    [] -> Head;
+    _ -> get_id(Tail)
+  end.
+
+get_name(List)->
+  [Head | Tail] = List,
+    
+  case "name" -- Head of
+    [] -> Head;
+    _ -> get_name(Tail)
+  end.
+
+get_id_from_the_list()->
+  application:ensure_all_started(hackney),
+  {ok, Body} = client_requests:get_simple_request_body(?URL++"graph/vertices"),
+  get_id(string:tokens(binary:bin_to_list(Body),"\"")).
+
+get_vertex_name(Id)->
+  application:ensure_all_started(hackney),
+  Url= ?URL++"graph/vertices?id="++Id,
+  {ok, Body} = client_requests:get_delete_request(Url, get),
+  NamePart = get_name(string:tokens(binary:bin_to_list(Body),",")), 
+  [_ | Tail] = string:tokens(NamePart, ":"),
+  [Head | _] = Tail,
+  Head -- "\"\"\\\\".
