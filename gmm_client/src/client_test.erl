199df8a3be84ed448784f5a237e046862246c9d9
Petros9 <piotrs1999@gmail.com>
2021-05-27 11:23:00 -0700

    add edge tests

--- gmm_client/src/client_test.erl
+++ gmm_client/src/client_test.erl
@@ -37,6 +37,41 @@ vertices_test()->
 
 
 edges_test()->
+  % given
+  client:add_user("Jan"),
+  client:add_user("Filip"),
+
+  JanId = client_test_helper:get_id_from_the_list(),
+  FilipId = client_test_helper:get_second_id_from_the_list(),
+
+  Permissions = "permissions",
+  Trace = "trace",
+  Successive = "successive",
+
+  NewPermissions = "new_permissions",
+
+  % when
+  client:add_edge(JanId, FilipId, Permissions, Trace, Successive),
+
+  % then
     [
-        ]
-    .
+      ?assertEqual("true", client_test_helper:check_existance(JanId, FilipId)),
+      ?assertEqual("\""++Permissions++"\"", client_test_helper:check_permissions(JanId, FilipId))
+    ],
+
+    % when
+    client:set_edge_permissions(JanId, FilipId, NewPermissions, Trace, Successive),
+
+    % then
+    [
+      ?assertEqual("\""++NewPermissions++"\"", client_test_helper:check_permissions(JanId, FilipId))
+    ],
+    % when
+    client:delete_edge(JanId, FilipId, Trace, Successive),
+
+    % then 
+    [
+      ?assertEqual("false", client_test_helper:check_existance(JanId, FilipId))
+    ],
+    client:delete_vertex(JanId),
+    client:delete_vertex(FilipId).
