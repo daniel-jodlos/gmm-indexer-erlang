f224189c9edcdad15a46ebc18456e5e4f3789b26
Paweł Marszał <45012218+MrPaulMarshall@users.noreply.github.com>
2021-06-24 18:43:56 +0200

    Gmmin 43 refactor utils and edges (#30)
    * Refactored gmm_utils and all but rest_edges
    
    * Module rest_edges successfully refactored
    
    * Fixed bug: taking values from wrong map in init/2
    
    * Quick fix to gmm_utils:create_vertex_id/2
    
    * quick fixes to tests
    
    * time change in ct
    
    Co-authored-by: Adrian <adrianwojciak@interia.pl>

--- src/rest/handlers/rest_edges.erl
+++ src/rest/handlers/rest_edges.erl
@@ -25,8 +25,9 @@
 %%%---------------------------
 
 init(Req0, State) ->
+    Operation = maps:get(operation, State),
     {ParsedParams, Req} =
-        case maps:get(operation, State) of
+        case Operation of
             Op when Op =:= add; Op =:= update ->
                 {cowboy_req:match_qs([
                     {from, nonempty}, {to, nonempty}, {permissions, nonempty},
@@ -42,6 +43,12 @@ init(Req0, State) ->
                 {ok, Map} = parse_bulk_request(ParsedJson),
                 {#{bulk_request => Map}, Req1}
         end,
+    if
+        Operation == add; Operation == update; Operation == delete ->
+            ok = gmm_utils:validate_vertex_id(maps:get(from, ParsedParams)),
+            ok = gmm_utils:validate_vertex_id(maps:get(to, ParsedParams));
+        true -> ok
+    end,
     NewState = maps:merge(State,
         case maps:find(successive, ParsedParams) of
             {ok, SuccessiveBin} ->
@@ -69,12 +76,15 @@ from_json(Req, State) ->
                 execute_operation(Op, From, To, Permissions, Trace, Successive);
             bulk ->
                 %% @todo execute bulk request parsed in init/2
-                ok
+                #{src_zone := _SourceZone, dst_zone := _DestinationZone, successive := _Successive, edges := _Edges} =
+                    maps:get(bulk_request, State),
+                execute_bulk_request()
         end,
-    Flag = case Result of
-               ok -> true;
-               {error, _} -> false
-           end,
+    Flag =
+        case Result of
+            ok -> true;
+            {error, _} -> false
+        end,
     {Flag, Req, State}.
 
 
@@ -84,11 +94,15 @@ from_json(Req, State) ->
 
 -spec parse_edge_string(binary()) -> {ok, map()} | {error, any()}.
 parse_edge_string(Bin) when is_binary(Bin) ->
-    case gmm_utils:split_bin(Bin) of
-        {ok, [From, To, Permissions, Trace]} ->
-            {ok, #{from => From, to => To, permissions => Permissions, trace => Trace}};
-        _ -> {error, "Incorrect edge string"}
-    end;
+    try
+        case gmm_utils:split_bin(Bin) of
+            [From, To, Permissions, Trace] when byte_size(From) > 0, byte_size(To) > 0, byte_size(Permissions) > 0 ->
+                ok = gmm_utils:validate_vertex_id(From),
+                ok = gmm_utils:validate_vertex_id(To),
+                {ok, #{from => From, to => To, permissions => Permissions, trace => Trace}};
+            _ -> {error, "Incorrect edge string"}
+        end
+    catch _:_ -> {error, "Invalid JSON"} end;
 parse_edge_string(_) ->
     {error, "Argument is not a binary"}.
 
@@ -100,7 +114,7 @@ parse_bulk_request(#{<<"sourceZone">> := SourceZone, <<"destinationZone">> := De
     case {gmm_utils:parse_boolean(SuccessiveBin), lists:all(fun({ok, _}) -> true; (_) -> false end, ParsedEdges)} of
         {{ok, Successive}, true} ->
             {_, ParsedList} = lists:unzip(ParsedEdges),
-            {ok, #{sourceZone => SourceZone, destinationZone => DestinationZone, successive => Successive, edges => ParsedList}};
+            {ok, #{src_zone => SourceZone, dst_zone => DestinationZone, successive => Successive, edges => ParsedList}};
         _ -> {error, "Invalid JSON"}
     end;
 parse_bulk_request(_) ->
@@ -108,62 +122,79 @@ parse_bulk_request(_) ->
 
 
 %%%---------------------------
-%% operation executor
+%% single operation executor
 %%%---------------------------
 
 -spec execute_operation(Op :: atom(), From :: binary(), To :: binary(), Permissions :: binary() | undefined,
     Trace :: binary(), Successive :: boolean()) -> ok | {error, any()}.
-execute_operation(Op, From, To, Permissions, Trace, false) ->
-    try
-        {ok, FromZone} = gmm_utils:owner_of(From),
-        ZoneId = gmm_utils:zone_id(),
-        if
-            %% @todo ZONE_ID is list "zone0", and not a binary <<"zone0">> - I need to do something about it
-            FromZone =/= ZoneId -> throw({return,
-                case Op of
-                    add -> zone_client:add_edge(FromZone, From, To, Permissions, Trace, false);
-                    update -> zone_client:set_permissions(FromZone, From, To, Permissions, Trace, false);
-                    delete -> zone_client:remove_edge(FromZone, From, To, Trace, false)
-                end});
-            true -> ok
+execute_operation(Op, From, To, Permissions, Trace, Successive) ->
+    VertexZone =
+        case Successive of
+            false -> gmm_utils:owner_of(From);
+            true -> gmm_utils:owner_of(To)
         end,
-        EdgeCond = (Op == delete) or (Op == update),
-        [{ok, true}, {ok, EdgeCond}] = [graph:vertex_exists(From), graph:edge_exists(From, To)],
-        ok = execute_operation(Op, From, To, Permissions, Trace, true),
-        case gmm_utils:owner_of(To) of
-            {ok, FromZone} -> ok; %% operation was already done on this zone in successive call
-            {ok, _} ->
-                case Op of
-                    add -> graph:create_edge(From, To, Permissions);
-                    update -> graph:update_edge(From, To, Permissions);
-                    delete -> graph:remove_edge(From, To)
-                end
-        end
-    catch
-        throw:{return, Result} -> Result;
-        _:_ -> {error, something}
-    end;
-execute_operation(Op, From, To, Permissions, Trace, true) ->
-    try
-        {ok, ToZone} = gmm_utils:owner_of(To),
-        ZoneId = gmm_utils:zone_id(),
-        if
-            ToZone =/= ZoneId -> throw({return,
-                case Op of
-                    add -> zone_client:add_edge(ToZone, From, To, Permissions, Trace, true);
-                    update -> zone_client:set_permissions(ToZone, From, To, Permissions, Trace, true);
-                    delete -> zone_client:remove_edge(ToZone, From, To, Trace, true)
-                end});
-            true -> ok
-        end,
-        EdgeCond = (Op == delete) or (Op == update),
-        [{ok, true}, {ok, EdgeCond}] = [graph:vertex_exists(To), graph:edge_exists(From, To)],
-        case Op of
-            add -> graph:create_edge(From, To, Permissions);
-            update -> graph:update_edge(From, To, Permissions);
-            delete -> graph:remove_edge(From, To)
-        end
-    catch
-        throw:{return, Result} -> Result;
-        _:_ -> {error, something}
+    ZoneId = gmm_utils:zone_id(),
+    case VertexZone of
+        ZoneId -> execute_locally(Op, From, To, Permissions, Trace, Successive);
+        _ -> redirect(VertexZone, Op, From, To, Permissions, Trace, Successive)
     end.
+
+
+-spec redirect(Zone :: binary(), Op :: atom(), From :: binary(), To :: binary(), Permissions :: binary(),
+    Trace :: binary(), Successive :: boolean()) -> ok | {error, any()}.
+redirect(Zone, Op, From, To, Permissions, Trace, Successive) ->
+    case Op of
+        add -> zone_client:add_edge(Zone, From, To, Permissions, Trace, Successive);
+        update -> zone_client:set_permissions(Zone, From, To, Permissions, Trace, Successive);
+        delete -> zone_client:remove_edge(Zone, From, To, Trace, Successive)
+    end.
+
+
+-spec execute_locally(Op :: atom(), From :: binary(), To :: binary(), Permissions :: binary(), Trace :: binary(),
+    Successive :: boolean()) -> ok | {error, any()}.
+execute_locally(Op, From, To, Permissions, Trace, false) ->
+    SuccessiveCallResult =
+        case conditions_met(Op, From, To, false) of
+            true -> execute_operation(Op, From, To, Permissions, Trace, true);
+            false -> {error, "Conditions failed"}
+        end,
+    case SuccessiveCallResult of
+        ok -> modify_state(Op, From, To, Permissions, false, gmm_utils:owner_of(From) == gmm_utils:owner_of(To));
+        {error, Reason} -> {error, Reason}
+    end;
+execute_locally(Op, From, To, Permissions, _Trace, true) ->
+    case conditions_met(Op, From, To, true) of
+        true -> modify_state(Op, From, To, Permissions, true, gmm_utils:owner_of(From) == gmm_utils:owner_of(To));
+        false -> {error, "Conditions failed"}
+    end.
+
+
+-spec conditions_met(Op :: atom(), From :: binary(), To :: binary(), Successive :: boolean()) -> boolean().
+conditions_met(Op, From, To, Successive) ->
+    EdgeCond = (Op == update) or (Op == delete),
+    Vertex = case Successive of false -> From; true -> To end,
+    case [graph:vertex_exists(Vertex), graph:edge_exists(From, To)] of
+        [{ok, true}, {ok, EdgeCond}] -> true;
+        _ -> false
+    end.
+
+
+-spec modify_state(Op :: atom(), From :: binary(), To :: binary(), Permissions :: binary(), Successive :: boolean(),
+    OneZoneOperation :: boolean()) -> ok | {error, any()}.
+modify_state(_, _, _, _, false, true) ->
+    ok;
+modify_state(add, From, To, Permissions, _, _) ->
+    graph:create_edge(From, To, Permissions);
+modify_state(update, From, To, Permissions, _, _) ->
+    graph:update_edge(From, To, Permissions);
+modify_state(delete, From, To, _, _, _) ->
+    graph:remove_edge(From, To).
+
+
+%%%---------------------------
+%% bulk operation executor
+%%%---------------------------
+
+-spec execute_bulk_request() -> ok | {error, any()}.
+execute_bulk_request() ->
+    {error, "Not implemented"}.
