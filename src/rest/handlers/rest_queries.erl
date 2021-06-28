cf12eb0ea7e73cce3459f9483d7472c1e9927c58
MrPaulMarshall <marszal.pawel@yahoo.com>
2021-06-28 22:49:21 +0200

    Fix to all this Daniels sh_t

--- src/rest/handlers/rest_queries.erl
+++ src/rest/handlers/rest_queries.erl
@@ -72,7 +72,7 @@ from_json(Req, State) ->
 
 
 %%%---------------------------
-%% internal functions
+%% Executor
 %%%---------------------------
 
 -spec execute(fun((...) -> {ok, any()} | {error, any()}), list(), binary()) -> {ok, map()} | {error, any()}.
@@ -91,7 +91,7 @@ execute(Fun, Args, FieldName) ->
         EndTime = erlang:timestamp(),
         Duration = timer:now_diff(EndTime, StartTime),
         case Result of
-%%            {error, Reason} -> {error, Reason};
+            {error, Reason} -> {error, Reason};
             {ok, Value} ->
                 {ok, #{<<"duration">> => gmm_utils:convert_microseconds_to_iso_8601(Duration), FieldName => Value}}
         end
@@ -99,12 +99,17 @@ execute(Fun, Args, FieldName) ->
         {error, "Execution error - probably wrong number of arguments"}
     end.
 
+
+%%%---------------------------
+%% Implementations
+%%%---------------------------
+
 %% @todo
 -spec reaches_naive(binary(), binary()) -> {ok, boolean()} | {error, any()}.
 reaches_naive(From, To) ->
-    case do_members_naive(To) of
+    case members_naive(To) of
         {error, Error} -> {error, Error};
-        {ok, Members} -> {ok, #{<<"reaches">> => sets:is_element(From, Members), <<"duration">> => 0}}
+        {ok, EffChildren} -> {ok, lists:member(From, EffChildren)}
     end.
 
 %% @todo
@@ -113,42 +118,47 @@ reaches_indexed(_From, _To) ->
     {ok, false}.
 
 %% @todo
--spec effective_permissions_naive(binary(), binary()) -> {ok, map()} | {error, any()}.
+-spec effective_permissions_naive(binary(), binary()) -> {ok, binary()} | {error, any()}.
 effective_permissions_naive(From, To) ->
-    MyZoneId = gmm_utils:zone_id(),
+    ZoneId = gmm_utils:zone_id(),
     case gmm_utils:owner_of(From) of
-        MyZoneId -> case do_effective_permissions_naive(From, To) of
-            {ok, Perms} -> {ok, #{<<"effectivePermissions">> => Perms, <<"duration">> => 0}};
-            {error, Error} -> {error, Error}
-        end;
-        Other -> zone_client:effective_permissions(naive, Other, From, To)
+        ZoneId -> effective_permissions_naive_locally(From, To);
+        Other ->
+            case zone_client:effective_permissions(naive, Other, From, To) of
+                {ok, #{<<"effectivePermissions">> := Perm}} -> {ok, Perm};
+                {error, Reason} -> {error, Reason}
+            end
     end.
 
--spec do_effective_permissions_naive(binary(), binary()) -> {ok, binary()} | {error, any()}.
-do_effective_permissions_naive(From, To) ->
+-spec effective_permissions_naive_locally(From :: binary(), To :: binary()) -> {ok, binary()} | {error, any()}.
+effective_permissions_naive_locally(From, To) ->
     JoinPermissions = fun
-        (<<"no-route">>, Permissions) -> Permissions;
-        (Permissions, <<"no-route">>) -> Permissions;
+%%        (<<"no-route">>, Permissions) -> Permissions;
+%%        (Permissions, <<"no-route">>) -> Permissions;
         (A, B) -> gmm_utils:permissions_or(A,B)
     end,
-    case graph:list_children(To) of
-        {ok, Children} ->
-            case lists:member(From, Children) of
-                true -> graph:get_edge(From, To);
-                false ->
-                    lists:foldl(fun
-                        (_, {error, Error}) -> {error, Error};
-                        (Child, {ok, Permissions}) ->
-                            case effective_permissions_naive(Child, To) of
-                                {ok, #{<<"effectivePermissions">> := ChildPermissions}} -> {ok, JoinPermissions(ChildPermissions, Permissions)};
-                                {error, Error} -> {error, Error}
-                            end
-                        end,
-                        {ok, <<"no-route">>},
-                        Children)
-            end;
-        {error, Error} -> {error, Error}
+    case graph:list_parents(From) of
+        {ok, Parents} ->
+            lists:foldr(
+                fun
+                    (_, {error, Reason}) -> {error, Reason};
+                    (To, {ok, Acc}) ->
+                        case graph:get_edge(From, To) of
+                            {error, Reason} -> {error, Reason};
+                            {ok, #{<<"permissions">> := Perm}} -> {ok, JoinPermissions(Perm, Acc)}
+                        end;
+                    (Parent, {ok, Acc}) ->
+                        case effective_permissions_naive(Parent, To) of
+                            {error, Reason} -> {error, Reason};
+                            {ok, Perm} -> {ok, JoinPermissions(Perm, Acc)}
+                        end
+                end,
+                {ok, <<"00000">>},
+                Parents
+            );
+        {error, Reason} -> {error, Reason}
     end.
+
 
 %% @todo
 -spec effective_permissions_indexed(binary(), binary()) -> {ok, binary()} | {error, any()}.
@@ -156,41 +166,35 @@ effective_permissions_indexed(_From, _To) ->
     {ok, <<"">>}.
 
 %% @todo
--spec members_naive(binary()) -> {ok, map()} | {error, any()}.
+-spec members_naive(binary()) -> {ok, list(binary())} | {error, any()}.
 members_naive(Of) ->
-    case do_members_naive(Of) of
-        {ok, Set} -> {ok, #{<<"members">> => sets:to_list(Set), <<"duration">> => 0}};
-        {error, Error} -> {error, Error}
-    end.
-
--spec do_members_naive(binary()) -> {ok, sets:set(binary())} | {error, any()}.
-do_members_naive(Of) ->
-    MyZoneId = gmm_utils:zone_id(),
+    ZoneId = gmm_utils:zone_id(),
     case gmm_utils:owner_of(Of) of
-        MyZoneId -> local_do_members_naive(Of);
+        ZoneId -> members_naive_locally(Of);
         Other ->
             case zone_client:members(naive, Other, Of) of
-                {ok, #{<<"members">> := Members}} -> {ok, sets:from_list(Members)}; 
-                {error, Error} -> {error, Error}
+                {ok, #{<<"members">> := Members}} -> {ok, Members};
+                {error, Reason} -> {error, Reason}
             end
     end.
 
--spec local_do_members_naive(binary()) -> {ok, sets:set()}.
-local_do_members_naive(Of) ->
+-spec members_naive_locally(Of :: binary()) -> {ok, list(binary())} | {error, any()}.
+members_naive_locally(Of) ->
     case graph:list_children(Of) of
         {ok, Children} ->
-            Res = lists:foldr(fun
-                (_, {error, Error}) -> {error, Error};
-                (Child, {ok, Acc}) ->
-                    case do_members_naive(Child) of
-                        {ok, Result} -> {ok, sets:union(Result, Acc)};
-                        A -> A
-                    end
+            Res = lists:foldr(
+                fun
+                    (_, {error, Error}) -> {error, Error};
+                    (Child, {ok, Acc}) ->
+                        case members_naive(Child) of
+                            {ok, Result} -> {ok, sets:union(sets:from_list(Result), Acc)};
+                            A -> A
+                        end
                 end,
                 {ok, sets:from_list(Children)},
                 Children),
             case Res of
-                {ok, SubChildren} -> {ok, sets:union(sets:from_list(Children), SubChildren)};
+                {ok, EffChildrenSet} -> {ok, sets:to_list(EffChildrenSet)};
                 {error, Error} -> {error, Error}
             end;
         {error, Error} -> {error, Error}
