63d4497d5f1636c1d71d81f41d346664ad1b65ed
Daniel Jodłoś <danjod40@gmail.com>
2021-07-17 17:20:11 +0200

    Event propagation

--- src/event_processor.erl
+++ src/event_processor.erl
@@ -12,14 +12,20 @@ graph_operation(added) -> fun graph:create_effective_edge/3;
 graph_operation(removed) -> fun ({A, B, _Args}) -> graph:remove_effective_edge(A,B) end;
 graph_operation(updated) -> fun graph:update_effective_edge/3.
 
--spec process(event()) -> ok | {error, any()}.
+-spec process(event()) -> {ok, [event()]} | {error, any()}.
 process({child, Operation, To, Child, Args}) ->
   GraphAction = graph_operation(Operation),
-  GraphAction(Child, To, Args);
+  GraphAction(Child, To, Args),
+  Parents = graph:list_parents(To),
+  Events = lists:map(fun (Parent) -> {child, Operation, Parent, Child, Args} end, Parents),
+  {ok, Events};
 
 process({parent, Operation, Target, Parent, Args}) ->
   GraphAction = graph_operation(Operation),
-  GraphAction(Target, Parent, Args);
+  GraphAction(Target, Parent, Args),
+  Children = graph:list_children(Target),
+  Events = lists:map(fun (Child) -> {parent, Operation, Child, Parent, Args} end, Children),
+  {ok, Events};
 
 process(_Event) ->
   {error, "Unhandled event"}.
