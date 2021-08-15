e1c4250c8ba18560c4cd3e84a5ee290cc39ccc7b
petros9 <piotrs1999@gmail.com>
2021-08-15 20:50:34 +0200

    improvement

--- src/rest/handlers/rest_edges.erl
+++ src/rest/handlers/rest_edges.erl
@@ -190,7 +190,7 @@ execute_bulk_request(Successive, Edges) ->
             Result = try
                 Params = string:split(Edge, "/", all),
                 [From, To, Permissions, Trace | _] = Params,
-                execute_operation(add, list_to_binary(From), list_to_binary(To), list_to_binary(Permissions), list_to_binary(Trace), list_to_binary(Successive))
+                execute_operation(add, list_to_binary(From), list_to_binary(To), Permissions, list_to_binary(Trace), list_to_binary(Successive))
             catch Type:Reason:Stacktrace ->
                 {'$pmap_error', self(), Type, Reason, Stacktrace}
             end,
