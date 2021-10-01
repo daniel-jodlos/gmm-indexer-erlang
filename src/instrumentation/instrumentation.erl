f891e996b843c8eb3a689f9eb73c47dcce308e5d
MrPaulMarshall <marszal.pawel@yahoo.com>
2021-10-01 02:16:52 +0200

    Implement writing to csv file

--- src/instrumentation/instrumentation.erl
+++ src/instrumentation/instrumentation.erl
@@ -10,17 +10,21 @@
 -export([
     create_ets/0,
     start_link/0,
+    init_writer/0,
     notify/1
 ]).
 
 -define(MAX_BATCH_SIZE, 10000).
+
+-include("records.hrl").
+
 
 %%%---------------------------
 %% Exported functions
 %%%---------------------------
 
 create_ets() ->
-    ets:new(instrumentation, [named_table]),
+    ets:new(instrumentation, [named_table, public]),
     ets:insert(instrumentation, {csv_writer, null}).
 
 start_link() ->
@@ -34,22 +38,22 @@ notify(Notification) ->
 %% Writer process routines
 %%%---------------------------
 
-read_blocking() ->
+read_blocking(File) ->
     receive
-        Notification -> read_nonblocking([Notification], 1)
+        Notification -> read_nonblocking(File, [Notification], 1)
     end.
 
-read_nonblocking(Batch, ?MAX_BATCH_SIZE) ->
-    write_batch(Batch, ?MAX_BATCH_SIZE);
-read_nonblocking(Batch, Size) ->
+read_nonblocking(File, Batch, ?MAX_BATCH_SIZE) ->
+    write_batch(File, Batch);
+read_nonblocking(File, Batch, Size) ->
     receive
-        Notification -> read_nonblocking([Notification | Batch], Size + 1)
-    after 0 -> write_batch(Batch, Size)
+        Notification -> read_nonblocking(File, [Notification | Batch], Size + 1)
+    after 0 -> write_batch(File, Batch)
     end.
 
-write_batch(Batch, Size) ->
-    csv_writer:write_lines(Batch, Size),
-    read_blocking().
+write_batch(File, Batch) ->
+    ok = csv_writer:write_lines(File, Batch),
+    read_blocking(File).
 
 
 %%%---------------------------
@@ -58,4 +62,5 @@ write_batch(Batch, Size) ->
 
 init_writer() ->
     ets:update_element(instrumentation, csv_writer, {2, self()}),
-    read_blocking().
+    {ok, File} = csv_writer:open_file(?CSV_FILE),
+    read_blocking(File).
