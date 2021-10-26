68917587654310837dd3f190bb59e1df174143f0
Adrian <adrianwojciak@interia.pl>
2021-10-26 19:16:40 +0200

    redis condition

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-redis-server /redis.conf --port 6379 2>&1 | sed 's/^/[redis] /' >&2 &
+if [ $REDIS != "false" ]
+then
+    redis-server /redis.conf --port 6379 2>&1 | sed 's/^/[redis] /' >&2 &
+fi
 
 /prod/bin/prod console
