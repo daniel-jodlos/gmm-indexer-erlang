e2ebc0147ed3aa8cb7cb1a039d4f1d9e1d094f14
Adrian <adrianwojciak@interia.pl>
2021-10-31 17:54:56 +0100

    delay

--- gmm_client/test/naive_queries_redis_outside_SUITE.erl
+++ gmm_client/test/naive_queries_redis_outside_SUITE.erl
@@ -29,7 +29,7 @@ end_per_suite(_Config) ->
     ok = application:stop(gmm_client).
 
 init_per_group(coverage, _Config) ->
-    timer:sleep(5000), %% for unknown reasons some delay is needed before executing first tests -
+    timer:sleep(10000), %% for unknown reasons some delay is needed before executing first tests -
                        %% without it tests fail with reason {badmatch, {error, closed}}
                        %% <<PAWEL>>: It seems that server is not ready immediately; {error, close} indicates,
                        %%            that hackney received error from tcp port
