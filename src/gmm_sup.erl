add7e56907c1f335516ae3ca339d48fc2ddce459
petros9 <piotrs1999@gmail.com>
2021-11-14 16:23:55 +0100

    possible solution v6

--- src/gmm_sup.erl
+++ src/gmm_sup.erl
@@ -53,7 +53,7 @@ init([]) ->
         intensity => 0,
         period => 1
     },
-    RedisSpecs = [create_redis_spec([], ?CLIENT_NUMBER) | outbox:specs_for_supervisor()],
+    RedisSpecs = create_redis_spec([], ?CLIENT_NUMBER),
     InboxDispatcherSpec = #{
         id => << "inbox_dispatcher" >>,
         start => {inbox, start_link, []}
@@ -62,5 +62,5 @@ init([]) ->
         id => <<"instrumentation">>,
         start => {instrumentation, start_link, []}
     },
-    ChildSpecs = [InstrumentationSpecs, InboxDispatcherSpec |  RedisSpecs],
+    ChildSpecs = lists:append([[InstrumentationSpecs, InboxDispatcherSpec], RedisSpecs, outbox:specs_for_supervisor()]),
     {ok, {SupFlags, ChildSpecs}}.
