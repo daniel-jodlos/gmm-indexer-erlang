e223465a3abc9fccb45dfdaa8ac6411d846c549b
Adrian <adrianwojciak@interia.pl>
2021-11-27 19:52:51 +0100

    binary

--- gmm_client/src/http_utils.erl
+++ gmm_client/src/http_utils.erl
@@ -33,13 +33,13 @@ get_address(_) ->
 
 -spec build_url(Address :: binary(), Path :: binary()) -> binary().
 build_url(Address, Path) ->
-    << Address/binary, "/", binary:replace(Path, <<" ">>, <<"%20">>, [global]) >>.
+    << Address/binary, "/", binary:replace(Path/binary, <<" ">>, <<"%20">>, [global]) >>.
 
 -spec build_url(Address :: binary(), Path :: binary(), Params :: list({binary(), binary()})) -> binary().
 build_url(Address, Path, []) ->
     build_url(Address, Path);
 build_url(Address, Path, [{FirstPar, FirstVal} | Rest]) ->
-    Base = << Address/binary, "/", binary:replace(Path, <<" ">>, <<"%20">>, [global]), "?", FirstPar/binary, "=", binary:replace(FirstVal, <<" ">>, <<"+">>, [global]) >>,
+    Base = << Address/binary, "/", binary:replace(Path/binary, <<" ">>, <<"%20">>, [global]), "?", FirstPar/binary, "=", binary:replace(FirstVal/binary, <<" ">>, <<"+">>, [global]) >>,
     lists:foldl(
-        fun({Param, Value}, Acc) -> << Acc/binary, "&", Param/binary, "=", binary:replace(Value, <<" ">>, <<"+">>, [global]) >> end,
+        fun({Param, Value}, Acc) -> << Acc/binary, "&", Param/binary, "=", binary:replace(Value/binary, <<" ">>, <<"+">>, [global]) >> end,
         Base, Rest).
