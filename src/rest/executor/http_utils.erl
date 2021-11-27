688bec394d28950fa8f638641dca1dbc6cb87ed3
Adrian <adrianwojciak@interia.pl>
2021-11-27 19:36:09 +0100

    maybe this

--- src/rest/executor/http_utils.erl
+++ src/rest/executor/http_utils.erl
@@ -35,7 +35,7 @@ get_port(Zone) ->
 
 -spec build_url(Address :: binary(), Path :: binary()) -> binary().
 build_url(Address, Path) ->
-    binary:replace(<< Address/binary, "/", Path/binary >>, <<" ">>, <<"+">>, [global]).
+    binary:replace(<< Address/binary, "/", Path/binary >>, <<" ">>, <<"%20">>, [global]).
 
 -spec build_url(Address :: binary(), Path :: binary(), Params :: list({binary(), binary()})) -> binary().
 build_url(Address, Path, []) ->
@@ -45,4 +45,4 @@ build_url(Address, Path, [{FirstPar, FirstVal} | Rest]) ->
     Url = lists:foldl(
         fun({Param, Value}, Acc) -> << Acc/binary, "&", Param/binary, "=", Value/binary >> end,
         Base, Rest),
-    binary:replace(Url, <<" ">>, <<"+">>, [global]).
+    binary:replace(Url, <<" ">>, <<"%20">>, [global]).
