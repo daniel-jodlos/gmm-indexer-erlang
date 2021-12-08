b3f00042445036c7161461008e8904034b2282a5
Pawel <pawel@ubuntu.ubuntu-domain>
2021-12-08 18:51:12 +0100

    Set indexation enabled by default
    Now if client wants to disable indexation,
    it needs to do it explicitly by making
    a request to /indexation endpoint
    (with body "false").

--- src/settings.erl
+++ src/settings.erl
@@ -38,7 +38,7 @@ create_ets() ->
 
     ets:new(?TABLE, [named_table, public, {read_concurrency, true}]),
     ets:insert(?TABLE, {?INDEXATION, true}),
-    ets:insert(?TABLE, {?INSTRUMENTATION, false}),
+    ets:insert(?TABLE, {?INSTRUMENTATION, true}),
     ets:insert(?TABLE, {?EVENTS_BATCH_SIZE, 5}),
     ets:insert(?TABLE, {?LOCAL_TESTS, LocalTests}).
 
